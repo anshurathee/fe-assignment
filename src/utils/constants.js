3e69936303d34bde5c5262e1c52ded2c270b6ac0
Anshuman Rathee <4nshu19@gmail.com>
2021-11-29 03:26:16 +0530

    Change base api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL =
-  'https://api.smarkets.com/v3/';
 // export const BASE_API_URL =
-//   'https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/';
+//   'https://api.smarkets.com/v3/';
+export const BASE_API_URL =
+  'https://cors-anywhere.herokuapp.com/https://api.smarkets.com/v3/';
